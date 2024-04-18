12d9f698cf5a1a3df467c80e8a51df1b3320d050
unknown <truongthienloc2@gmail.com>
2024-04-18 20:38:17 +0700

    feature(feedback): handle feedback and modify navbar

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: '/',
+        destination: '/coding',
+        permanent: true,
+      },
+      
+    ]
+  },
 }
 
 module.exports = nextConfig
